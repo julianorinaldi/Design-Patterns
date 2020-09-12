a2810603313892af9cf584f44a9ae0a059293ab9
JulianoRinaldi <julianorinaldi@gmail.com>
2020-09-12 15:28:15 -0300

    Builder

--- CreationalPatterns/Builder/Chuteira.cs
+++ CreationalPatterns/Builder/Chuteira.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DesignPatterns.CreationalPatterns.Builder
+﻿namespace DesignPatterns.CreationalPatterns.Builder
 {
     public class Chuteira : ITenisBuilder
     {
