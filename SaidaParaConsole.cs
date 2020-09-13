c6284247d61b289156db77cf3daf70e30a4302f4
JulianoRinaldi <julianorinaldi@gmail.com>
2020-09-13 14:41:31 -0300

    Output

---
+++ SaidaParaConsole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    public class SaidaParaConsole : ISaidaDeTexto
+    {
+        public void EscreverTexto(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+        }
+    }
+}
