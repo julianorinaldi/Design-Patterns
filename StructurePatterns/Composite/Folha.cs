fd2b2db5dfc0ebf168fa3dd9ac265b32c32667fe
JulianoRinaldi <julianorinaldi@gmail.com>
2020-09-13 16:37:44 -0300

    Composite

--- StructurePatterns/Composite/Folha.cs
+++ StructurePatterns/Composite/Folha.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DesignPatterns.StructurePatterns.Composite
+﻿namespace DesignPatterns.StructurePatterns.Composite
 {
     /// <summary>
     /// Faz o papel do Leaf/Folha
