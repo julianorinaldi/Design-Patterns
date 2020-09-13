e4c52ac49c120042726489d9322ba0ba3fd3a64a
JulianoRinaldi <julianorinaldi@gmail.com>
2020-09-13 16:38:01 -0300

    Bridge

--- StructurePatterns/Brigde/TipoPapelEnum.cs
+++ StructurePatterns/Brigde/TipoPapelEnum.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DesignPatterns.StructurePatterns.Brigde
+﻿namespace DesignPatterns.StructurePatterns.Brigde
 {
     public enum TipoPapelEnum
     {
