a4392ff474c001cb6c657220ac688909c72a6275
JulianoRinaldi <julianorinaldi@gmail.com>
2020-09-18 16:46:04 -0300

    Prototype

---
+++ DesignPatternsLibrary/CreationalPatterns/Prototype/IDado.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DesignPatterns.CreationalPatterns.Prototype
+{
+    public interface IDado : ICloneable
+    {
+        // No .net existe a interface `ICloneable` que contém um método `object Clone();`
+        // Que já representa a formulação do padrão.
+    }
+}
