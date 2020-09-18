6aa05736b96b6e5c1b241fcae20dc325cd2f41a0
JulianoRinaldi <julianorinaldi@gmail.com>
2020-09-18 16:46:42 -0300

    Adapter

---
+++ DesignPatternsLibrary/StructurePatterns/Adapter/NotaFiscalJSONAdapter.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.StructurePatterns.Adapter
+{
+    public class NotaFiscalJSONAdapter : NotaFiscalXML
+    {
+        private readonly NotaFiscalXML _notaFiscalXML;
+
+        public NotaFiscalJSONAdapter(NotaFiscalXML notaFiscalXML) : base(notaFiscalXML.NumeroNota)
+        {
+            _notaFiscalXML = notaFiscalXML;
+        }
+
+        public override string GerarNotaFiscal()
+        {
+            return @$"xml {{ numeroNota: {_notaFiscalXML.NumeroNota} }}";
+        }
+    }
+}
---
+++ DesignPatternsLibrary/StructurePatterns/Adapter/NotaFiscalXML.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.StructurePatterns.Adapter
+{
+    public class NotaFiscalXML
+    {
+        public string NumeroNota { get; private set; }
+
+        public NotaFiscalXML(string numeroNota)
+        {
+            NumeroNota = numeroNota;
+        }
+
+        public virtual string GerarNotaFiscal()
+        {
+            return @$"<xml><numeroNota>{NumeroNota}</numeroNota></xml>";
+        }
+    }
+}
