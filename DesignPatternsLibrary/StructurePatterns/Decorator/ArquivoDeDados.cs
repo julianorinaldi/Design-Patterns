a0c9aef90675363958b0f7b54c1ed5a445efe9d2
JulianoRinaldi <julianorinaldi@gmail.com>
2020-09-18 16:47:07 -0300

    Decorator

---
+++ DesignPatternsLibrary/StructurePatterns/Decorator/ArquivoDeDados.cs
@@ -0,0 +1,22 @@
+﻿namespace DesignPatterns.StructurePatterns.Decorator
+{
+    public class ArquivoDeDados : IDados
+    {
+        public string ConteudoDoArquivo { get; private set; }
+
+        public ArquivoDeDados(string conteudoDoArquivo)
+        {
+            ConteudoDoArquivo = conteudoDoArquivo;
+        }
+
+        public void EscreverDados(string text)
+        {
+            ConteudoDoArquivo = text;
+        }
+
+        public string LerDados()
+        {
+            return ConteudoDoArquivo;
+        }
+    }
+}
