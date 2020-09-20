c38090628bdb97d3d1f504337cc2c136f2579f29
JulianoRinaldi <julianorinaldi@gmail.com>
2020-09-20 09:46:45 -0300

    ChainOfResposability

--- DesignPatternsLibrary/BehavioralPatterns/ChainOfResponsability/TratamentoInternacional.cs
+++ DesignPatternsLibrary/BehavioralPatterns/ChainOfResponsability/TratamentoInternacional.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DesignPatterns.BehavioralPatterns.ChainOfResponsability
+﻿namespace DesignPatterns.BehavioralPatterns.ChainOfResponsability
 {
     public class TratamentoInternacional : FiscalizadorBagagem
     {
